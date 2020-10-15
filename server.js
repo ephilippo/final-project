a7e6b4d8c70a7924ab799eb83b60ef01ab472394
Glitch (bong-p) <none>
2020-10-15 20:59:05 +0000

    MERGED GAME W/ UI

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const app = express();
 
 app.use(express.static("public"));
 
+//include multipage viewing - sadie
+app.use(express.static(__dirname + '/views'));
+
 // https://expressjs.com/en/starter/basic-routing.html
 app.get("/", (request, response) => {
   response.sendFile(__dirname + "/views/index.html");
